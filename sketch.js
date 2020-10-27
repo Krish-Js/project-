47687e522afb76ad6f6fc0c79cee97d4ce257513
Krish-Js <72423844+Krish-Js@users.noreply.github.com>
2020-10-26 21:02:33 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -13,27 +13,27 @@ wall.shapeColor=(80,80,80);
 //if(car.isTouching(wall));
 //car.velocityX=0;
 
-if(wall.x-car.x < (car.width+wall.width)/2){
-  car.velocityX=0;
-  var deformation=0.5 * weight * speed *speed/22500;
-  if(car.isTouching(wall) && deformation>180)
-  {
-    car.shapeColor="red";
-  }
-  if(car.isTouching(wall) && deformation<180 && deformation>100)
-  {
-      car.shapeColor="yellow";
-  }
-  if(car.isTouching(wall) && deformation<100){
-  car.shapeColor="green";
-  }
 
-  }  
-}
 
 function draw() {
   background(255,255,255);  
+  if(wall.x-car.x < (car.width+wall.width)/2){
+    car.velocityX=0;
+    var deformation=0.5 * weight * speed *speed/22500;
+    if(car.isTouching(wall) && deformation>180)
+    {
+      car.shapeColor="red";
+    }
+    if(car.isTouching(wall) && deformation<180 && deformation>100)
+    {
+        car.shapeColor="yellow";
+    }
+    if(car.isTouching(wall) && deformation<100){
+    car.shapeColor="green";
+    }
   
+    }  
+  }
   drawSprites();
 
 }
